4efeebfa1b65545c5144e4547ac3d8da60cc323b
vovinov <v.v.ovinov@yandex.ru>
2024-06-13 11:47:25 +0300

    66_plus_one final solution

--- _Leetcode/66_Plus_One.py
+++ _Leetcode/66_Plus_One.py
@@ -8,13 +8,21 @@
 from collections import deque
 
 def plusOne(digits):
-  stack = []
-  for d in digits:
-    ...
-    stack.append(d)
+    for i in range(len(digits) - 1, -1, -1):
+       if digits[i] != 9:
+           digits[i] += 1
+           return digits
+       else: 
+           digits[i] = 0
+    digits.insert(0, 1)
+    return digits
+
+            
+
+  
 
 
 #print(plusOne([1,2,3])) # [1, 2, 4]
-#print(plusOne([4,3,2,1]))  # [4,3,2,2]
+print(plusOne([4,3,2,1]))  # [4,3,2,2]
 #print(plusOne([9])) # [1,0]
 print(plusOne([9, 9])) # [1,0,0]
