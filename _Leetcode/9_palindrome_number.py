7a05f356e487a71592448f582884451ee9b77875
vovinov <v.v.ovinov@yandex.ru>
2024-06-26 13:56:39 +0300

    Add 217 solution

--- _Leetcode/9_palindrome_number.py
+++ _Leetcode/9_palindrome_number.py
@@ -1,7 +1,31 @@
-def isPalindrome(x: int):
+'''
+Given an integer x, return true if x is a palindrome, and false otherwise.
+'''
+
+# Solution1
+def isPalindrome1(x: int):
     
     return str(x) == str(x)[::-1]
 
 
-isPalindrome(10)
-# 121
+# Solution2
+def isPalindrome2(x: int):
+
+    if x < 0:
+        return False
+    
+    new = 0
+    orig = x
+
+    while x:
+
+        x, d = divmod(x, 10)
+
+        new = new * 10 + d
+
+    return new == orig
+
+
+print(isPalindrome2(121))
+print(isPalindrome2(-121))
+print(isPalindrome2(10))
---
+++ linked_list/203_Remove_Linked_List_Elements.py
@@ -0,0 +1,14 @@
+'''
+Given the head of a linked list and an integer val,
+remove all the nodes of the linked list that has
+Node.val == val, and return the new head.
+'''
+
+
+def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+    pass
+
+
+print([1,2,6,3,4,5,6], 6)
+print([], 1)
+print([7,7,7,7], 7)
