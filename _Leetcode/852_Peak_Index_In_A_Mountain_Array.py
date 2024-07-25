2e4cee11681b6a61b97eb709b6a9e93f341d81fb
vovinov <v.v.ovinov@yandex.ru>
2024-07-25 15:47:24 +0300

    Add 852 solution

--- _Leetcode/852_Peak_Index_In_A_Mountain_Array.py
+++ _Leetcode/852_Peak_Index_In_A_Mountain_Array.py
@@ -8,9 +8,28 @@ from typing import List
 
 
 def peakIndexInMountainArray(arr: List[int]) -> int:
-    pass
+    
+    l, r = 0, len(arr) - 1
+    
+    while l <= r:
+
+        mid = (l + r) // 2
+
+        if arr[mid - 1] < arr[mid] > arr[mid + 1]:
+            return mid
+        
+        if arr[mid] > arr[mid - 1]:
+
+            l = mid
+
+        else:
+            r = mid
+
+    return mid
 
 
-print(peakIndexInMountainArray([0, 1, 0]))
-print(peakIndexInMountainArray([0, 2, 1, 0]))
-print(peakIndexInMountainArray([0, 10, 5, 2]))
+#  print(peakIndexInMountainArray([0, 1, 0]))
+#  print(peakIndexInMountainArray([0, 2, 1, 0]))
+#  print(peakIndexInMountainArray([0, 10, 5, 2]))
+#  print(peakIndexInMountainArray([3, 4, 5, 1]))
+print(peakIndexInMountainArray([40, 48, 61, 75, 100, 99, 98, 39, 30, 10]))
--- _Leetcode/929_Unique_email_addresses.py
+++ _Leetcode/929_Unique_email_addresses.py
@@ -12,13 +12,13 @@ def umUniqueEmails(emails):
     res = []
     for email in emails:
         email = email.split('@')
-        email[0] = email[0].replace('.','').split('+')[0]
+        email[0] = email[0].replace('.', '').split('+')[0]
         res.append('@'.join(email))
 
     return len(set(res))
 
 
-print( 
+print(
     umUniqueEmails(
         [
             "test.email+alex@leetcode.com",
@@ -26,5 +26,7 @@ print(
             "testemail+david@lee.tcode.com",
         ]
     )
-) # 2
-print(umUniqueEmails(["a@leetcode.com", "b@leetcode.com", "c@leetcode.com"])) # 3 
+)  # 2
+print(umUniqueEmails(["a@leetcode.com",
+                      "b@leetcode.com",
+                      "c@leetcode.com"]))  # 3
