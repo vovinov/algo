cfc40f14bc43dfa94a85e647bf1d7953be82bda3
vovinov <v.v.ovinov@yandex.ru>
2024-11-05 20:38:06 +0300

    Add another solution to ex 26

--- _Leetcode/26_Remove_Duplicate_From_Sorted_Array.py
+++ _Leetcode/26_Remove_Duplicate_From_Sorted_Array.py
@@ -16,5 +16,18 @@ def removeDuplicates(nums):
             two += 1
 
 
+def removeDuplicates2(nums):
+
+    j = 0
+
+    for i in range(len(nums)):
+
+        if nums[i] != nums[j]:
+            j += 1
+            nums[i], nums[j] = nums[j], nums[i]
+
+    return j + 1
+
+
 print(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
-assert removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]) == 5
+print(removeDuplicates2([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
