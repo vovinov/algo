71b56e761c58c14df94c626b5f5b837b1a8347b8
vovinov <v.v.ovinov@yandex.ru>
2024-11-06 16:19:13 +0300

    Update 205 ex solution

--- _Leetcode/205_Isomorphic_Strings.py
+++ _Leetcode/205_Isomorphic_Strings.py
@@ -1,29 +1,46 @@
-'''
-Given two strings s and t, determine if they are isomorphic.
-Two strings s and t are isomorphic if the characters in s can be replaced to get t.
-'''
-
-def isIsomorphic(s: str, t: str):
+def isIsomorphic1(s: str, t: str):
 
     s2t, t2s = {}, {}
-
 
     for c1, c2 in zip(s, t):
 
         if c1 in s2t and s2t[c1] != c2:
             return False
-        
+
         if c2 in t2s and t2s[c2] != c1:
             return False
-        
+
         s2t[c1] = c2
         t2s[c2] = c1
 
     return True
-    
 
 
+def isIsomorphic2(s: str, t: str):
 
-print(isIsomorphic("egg", "add"))
+    s2t = {}
+    t2s = {}
+
+    for i in range(len(s)):
+        cur_s = s[i]
+        cur_t = t[i]
+
+        # a -> e
+        if cur_s in s2t:
+            if s2t[cur_s] != cur_t:
+                return False
+        else:
+            # e -> a
+            if cur_t in t2s and t2s[cur_t] != cur_s:
+                return False
+            else:
+                s2t[cur_s] = cur_t
+                t2s[cur_t] = cur_s
+
+    return True
+
+
+print(isIsomorphic1("egg", "add"))
+print(isIsomorphic2("egg", "add"))
 # print(isIsomorphic("foo", "bar"))
 # print(isIsomorphic("paper", "title"))
